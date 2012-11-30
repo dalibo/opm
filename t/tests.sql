b814f8da9ce329a11f883e286be12107a328c073
Thomas Reiss <thomas.reiss@dalibo.com>
2012-11-30 17:14:11 +0100

    Modify t/tests.sql to use the PF extension and complete the test suite

--- t/tests.sql
+++ t/tests.sql
@@ -2,9 +2,15 @@
 \timing off
 \t on
 
+\set admincluster postgres
+
 \set dbfactory pgfactory
 \set ownerfactory pgfactory
 \set adminfactory pgf_admins
+\set userfactory user1
+
+DROP DATABASE IF EXISTS :dbfactory;
+CREATE DATABASE :dbfactory;
 
 \c :dbfactory
 
@@ -17,8 +23,22 @@ DROP ROLE IF EXISTS "user1";
 DROP ROLE IF EXISTS "user2";
 DROP ROLE IF EXISTS "user3";
 DROP ROLE IF EXISTS "userN";
+DROP ROLE IF EXISTS pgf_accounts;
+DROP ROLE IF EXISTS pgf_admins;
+DROP ROLE IF EXISTS pgfactory;
+
 
 TRUNCATE public.roles CASCADE;
+
+SELECT '====Install pgfactory and wh_nagios====';
+CREATE EXTENSION "pgfactory-core";
+CREATE EXTENSION hstore;
+CREATE EXTENSION wh_nagios;
+
+SELECT '====Create some fake services====';
+INSERT INTO services (hostname, warehouse, service) VALUES ('barbapapa1', 'wh_nagios', 'Service1');
+INSERT INTO services (hostname, warehouse, service) VALUES ('barbapapa2', 'wh_nagios', 'Service1');
+INSERT INTO services (hostname, warehouse, service) VALUES ('barbapapa2', 'wh_nagios', 'Service2');
 
 -- normal tests
 SET ROLE :adminfactory;
@@ -33,6 +53,9 @@ SELECT * FROM create_user('user3', 'password1', '{acc1}');
 SELECT * FROM create_user('user2', 'password2', '{acc2}');
 SELECT * FROM create_user('userN', 'passwordN', '{acc1, acc2}');
 
+SELECT '====Grant user1 to access service id=1====';
+SELECT * FROM grant_service(1, 'user1');
+
 SELECT '====Roles réels====';
 \du
 
@@ -44,10 +67,29 @@ SELECT * FROM public.list_users();
 SELECT '====Table public.roles: permission denied====';
 SELECT * FROM public.roles;
 
+SELECT '====Table public.services: permission denied====';
+SELECT * FROM public.services;
+
+SELECT '====What admin sees====';
+SELECT * FROM public.list_services();
+
+\c :dbfactory :userfactory;
+
+SELECT '====What user1 sees====';
+SELECT * FROM public.list_services();
+
+SELECT * FROM wh_nagios.hub;
+
+--SET ROLE :adminfactory;
+\c :dbfactory :admincluster;
+
 -- cleanup
 SELECT '====Cleanup====';
 
 SELECT drop_account('acc1');
 SELECT drop_account('acc2');
 
+SET ROLE :admincluster;
 
+DROP EXTENSION wh_nagios;
+DROP EXTENSION "pgfactory-core";
