9ad23a9f803f490b02524b4a7a7c498cbc6d3020
Julien Rouhaud <julien.rouhaud@free.fr>
2013-05-17 15:34:02 +0200

    Add link between graphs and services.
    This allow easy automatic graph generation. UI has to know how to handle
    a specific warehouse to display the graph.
    A graph can have one ore more service, and a service can be in one or
    more graph.

--- processes/pr_grapher/pr_grapher--1.0.sql
+++ processes/pr_grapher/pr_grapher--1.0.sql
@@ -61,6 +61,16 @@ CREATE TABLE pr_grapher.series (
 ALTER TABLE pr_grapher.series OWNER TO pgfactory;
 REVOKE ALL ON TABLE pr_grapher.series FROM public;
 
+-- A graph can display one or more services
+CREATE TABLE pr_grapher.graph_services (
+  id_graph bigint not null references pr_grapher.graphs (id),
+  id_service bigint not null
+);
+--TODO: add constraint trigger to enforce FK pr_grapher.graph_services and public.services
+
+ALTER TABLE pr_grapher.graph_services ADD PRIMARY KEY (id_graph, id_service);
+ALTER TABLE pr_grapher.graph_services OWNER TO pgfactory;
+REVOKE ALL ON TABLE pr_grapher.graph_services FROM public;
 
 -- jstime: Convert the input date to ms from the Epoch in UTC, suitable for javascript
 CREATE OR REPLACE FUNCTION pr_grapher.js_time(timestamptz) RETURNS bigint LANGUAGE 'sql' IMMUTABLE SECURITY DEFINER
@@ -81,3 +91,54 @@ WITH RECURSIVE tc(id, category, description, distance, path, cycle) AS (
   SELECT c.id, c.category, c.description, tc.distance + 1, path || c.id, c.id = ANY(path) FROM pr_grapher.categories c LEFT JOIN pr_grapher.nested_categories n ON (c.id = n.id_child) JOIN tc ON (tc.id = n.id_parent) WHERE NOT tc.cycle
 ) SELECT id, category, description, distance, path FROM tc WHERE NOT cycle ORDER BY path;
 $$;
+
+CREATE OR REPLACE FUNCTION pr_grapher.create_graph_for_services(OUT rc boolean)
+AS $$
+DECLARE
+  v_state   TEXT;
+  v_msg     TEXT;
+  v_detail  TEXT;
+  v_hint    TEXT;
+  v_context TEXT;
+  servicesrow public.services%rowtype;
+BEGIN
+  FOR servicesrow IN (SELECT s.* FROM public.services s
+    LEFT JOIN pr_grapher.graph_services gs ON gs.id_service = s.id
+    WHERE gs.id_service IS NULL)
+  LOOP
+    WITH new_graphs (id_graph) AS (
+      INSERT INTO pr_grapher.graphs (graph, config)
+        VALUES (servicesrow.service, '{"type": "lines"}')
+        RETURNING graphs.id
+    )
+    INSERT INTO pr_grapher.graph_services (id_graph, id_service)
+      SELECT new_graphs.id_graph, servicesrow.id
+      FROM new_graphs;
+  END LOOP;
+  rc := true;
+EXCEPTION
+  WHEN OTHERS THEN
+    GET STACKED DIAGNOSTICS
+      v_state   = RETURNED_SQLSTATE,
+      v_msg     = MESSAGE_TEXT,
+      v_detail  = PG_EXCEPTION_DETAIL,
+      v_hint    = PG_EXCEPTION_HINT,
+      v_context = PG_EXCEPTION_CONTEXT;
+    raise notice E'Unhandled error on pr_grapher.create_graph_for_services:
+      state  : %
+      message: %
+      detail : %
+      hint   : %
+      context: %', v_state, v_msg, v_detail, v_hint, v_context;
+    rc := false;
+END;
+$$
+LANGUAGE plpgsql
+LEAKPROOF
+SECURITY DEFINER;
+
+ALTER FUNCTION pr_grapher.create_graph_for_services(OUT rc boolean) OWNER TO pgfactory;
+REVOKE ALL ON FUNCTION pr_grapher.create_graph_for_services(OUT rc boolean) FROM public;
+GRANT ALL ON FUNCTION pr_grapher.create_graph_for_services(OUT rc boolean) TO pgf_admins;
+
+COMMENT ON FUNCTION pr_grapher.create_graph_for_services(OUT rc boolean) IS 'Create default graphs for all new services.'
