267ae14ed0e78c0712a83283dfc9b108743e28c2
Julien Rouhaud <julien.rouhaud@free.fr>
2013-05-17 20:39:10 +0200

    Fix prepared statement paremeter.

--- ui/pg_factory/lib/PgFactory/Server.pm
+++ ui/pg_factory/lib/PgFactory/Server.pm
@@ -55,8 +55,10 @@ sub host{
 
     $sql = $dbh->prepare("SELECT pr_grapher.create_graph_for_services(id) FROM public.list_servers() WHERE hostname = ?");
     $sql->execute($hostname);
-    $sql = $dbh->prepare("SELECT s1.id,s1.warehouse,s1.service,s1.last_modified,s1.creation_ts,s1.servalid, g.id, g.graph FROM public.list_services() s1 JOIN public.list_servers() s2 ON s2.id = s1.id_server JOIN pr_grapher.graph_services gs ON gs.id_service = s1.id JOIN pr_grapher.graphs g ON g.id = gs.id_graph WHERE s2.hostname = '$hostname';");
-    $sql->execute();
+    $sql->finish();
+
+    $sql = $dbh->prepare("SELECT s1.id,s1.warehouse,s1.service,s1.last_modified,s1.creation_ts,s1.servalid, g.id, g.graph FROM public.list_services() s1 JOIN public.list_servers() s2 ON s2.id = s1.id_server JOIN pr_grapher.graph_services gs ON gs.id_service = s1.id JOIN pr_grapher.graphs g ON g.id = gs.id_graph WHERE s2.hostname = ?;");
+    $sql->execute($hostname);
     my $services = [];
     while (my ($id,$warehouse,$service,$last_mod,$creation_ts,$servalid,$id_graph,$graphname) = $sql->fetchrow()){
         push @{$services},{ id => $id, warehouse => $warehouse, servicename => $service, lst_mod => $last_mod, creation_ts => $creation_ts, servalid => $servalid, id_graph => $id_graph, graphname => $graphname};
