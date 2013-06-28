953e05b521df7ad6e3f04ce171e90149b996644c
Jehan-Guillaume (ioguix) de Rorthais <jgdr@dalibo.com>
2013-06-28 15:41:53 +0200

    UI: uniformize style

--- ui/pg_factory/modules/grapher/public/js/show.js
+++ ui/pg_factory/modules/grapher/public/js/show.js
@@ -2,24 +2,25 @@
  * Javascript for page show.html.ep
  **/
 $(document).ready(function () {
-  $('.scales input[type=button]').click(function () {
+  $('.scales .btn').click(function (e) {
     var fromDate = new Date();
     var toDate = new Date();
 
-    switch($(this).attr('value')) {
-      case 'year':
+    //switch($(this).attr('value')) {
+    switch($(this).attr('id')) {
+      case 'sel_year':
           fromDate.setYear(fromDate.getYear() + 1900 - 1);
         break;
-        case 'month':
+        case 'sel_month':
           fromDate.setMonth(fromDate.getMonth() - 1);
         break;
-        case 'week':
+        case 'sel_week':
           fromDate.setDate(fromDate.getDate() - 7);
         break;
-        case 'day':
+        case 'sel_day':
           fromDate.setDate(fromDate.getDate() - 1);
         break;
-        case 'custom':
+        case 'sel_custom':
           if ($('#fromdate').attr('value') === '' ) {
             alert('you must set the starting date.');
             return false;
@@ -39,7 +40,7 @@ $(document).ready(function () {
     $('[id-graph]').grapher({from: fromDate.getTime(), to: toDate.getTime(), url: "/grapher/graphs/data" });
   })
   /* by default, show the week graph by triggering the week button */
-  $('ul.scales input[value=week]').click();
+  $('#sel_week').click();
 
 
   /* bind the datepicker to the date fields */
