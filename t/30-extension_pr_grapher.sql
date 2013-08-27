7faec52c9bb64ad8f76ae2a5709c9ed6b023fbfb
Julien Rouhaud <julien.rouhaud@free.fr>
2013-08-27 15:48:22 +0200

    [test]: add few more tests for pr_grapher extension.

--- t/30-extension_pr_grapher.sql
+++ t/30-extension_pr_grapher.sql
@@ -1,7 +1,7 @@
 \unset ECHO
 \i t/setup.sql
 
-SELECT plan(25);
+SELECT plan(28);
 
 SELECT diag(E'\n==== Setup environnement ====\n');
 
@@ -41,11 +41,33 @@ SELECT has_function('pr_grapher', 'js_time', '{timestamp with time zone}', 'Func
 SELECT has_function('pr_grapher', 'get_categories', '{}', 'Function "pr_grapher.get_categories" exists.');
 SELECT has_function('pr_grapher', 'list_graph', '{}', 'Function "pr_grapher.list_graph" exists.');
 
+SELECT set_eq($$SELECT * from pr_grapher.js_time('2013-01-01 12:34:56 CEST')$$,
+    $$VALUES (1357040096000)$$,
+    'Test js_time function.'
+);
+
 SELECT diag(E'\n==== Test ACl ====\n');
 
 SELECT set_eq($$SELECT COUNT(*) FROM pr_grapher.list_graph()$$,
     $$VALUES (0)$$,
     'Should not see any graph.'
+);
+
+SELECT lives_ok($$INSERT INTO pr_grapher.graphs (graph,description,config) VALUES
+    ('Test graph 1','A simple graph test','{}'::json)$$,
+    'Insert an empty graph'
+);
+
+SELECT set_eq($$SELECT id,graph,description,y1_query,y2_query,config::text  FROM pr_grapher.list_graph()$$,
+    $$VALUES (1::bigint,'Test graph 1','A simple graph test',NULL,NULL,'{}')$$,
+    'Should see one graph.'
+);
+-- As PG 9.2 can't compare two json, we need to test the return args
+SELECT set_eq($$SELECT proallargtypes,proargmodes,proargnames FROM pg_proc p
+    JOIN pg_namespace n ON p.pronamespace = n.oid
+    WHERE proname = 'list_graph' AND nspname = 'pr_grapher'$$,
+    $$VALUES ('{20,25,25,25,25,114}'::oid[],'{t,t,t,t,t,t}'::"char"[],'{id,graph,description,y1_query,y2_query,config}'::text[])$$,
+    'Return arguments of function list_graph of schema "pr_grapher" should be correct.'
 );
 
 SELECT diag(E'\n==== Drop pr_grapher ====\n');
@@ -72,7 +94,6 @@ SELECT hasnt_table('pr_grapher', 'series',
     'Table "series" of schema "pr_grapher" should not exists anymore.'
 );
 
-SELECT hasnt_function('pr_grapher', 'create_graph_for_services', '{bigint}', 'Function "pr_grapher.create_graph_for_services" does not exists.');
 SELECT hasnt_function('pr_grapher', 'js_time', '{timestamp with time zone}', 'Function "pr_grapher.js_time" does not exists.');
 SELECT hasnt_function('pr_grapher', 'get_categories', '{}', 'Function "pr_grapher.get_categories" does not exists.');
 SELECT hasnt_function('pr_grapher', 'list_graph', '{}', 'Function "pr_grapher.list_graph" does not exists.');
